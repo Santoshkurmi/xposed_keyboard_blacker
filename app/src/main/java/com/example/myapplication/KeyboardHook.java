05a86bdaa66cd0e43ee97b372443afdd7921363a
Santosh kurmi <xantosh121@gmail.com>
2025-01-26 20:34:18 +0545

    Added blacker for latest gboard

--- app/src/main/java/com/example/myapplication/KeyboardHook.java
+++ app/src/main/java/com/example/myapplication/KeyboardHook.java
@@ -17,16 +17,26 @@ public class KeyboardHook implements IXposedHookLoadPackage {
             XposedBridge.log("Starting the gboard");
 
        Class<?> keyboard =  XposedHelpers.findClass("com.google.android.libraries.inputmethod.keyboard.impl.KeyboardViewHolder",loadPackageParam.classLoader);
-       XposedHelpers.findAndHookMethod(keyboard, "addView", View.class,int.class,ViewGroup.LayoutParams.class, new XC_MethodHook() {
-           @Override
-           protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-               super.afterHookedMethod(param);
-//               XposedBridge.log("Making it transparent");
-               View view = (View) param.args[0];
-               if( ((ViewGroup)view).getChildCount() ==2)  view.setAlpha(0.0f);
+//       XposedHelpers.findAndHookMethod(keyboard, "addView", View.class,int.class,ViewGroup.LayoutParams.class, new XC_MethodHook() {
+//           @Override
+//           protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//               super.afterHookedMethod(param);
+////               XposedBridge.log("Making it transparent");
+//               View view = (View) param.args[0];
+//               if( ((ViewGroup)view).getChildCount() ==2)  view.setAlpha(0.0f);
+//
+//
+//           }//fter hok
+//       });//find and hook method
 
+        XposedBridge.hookAllMethods(keyboard, "g", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                View view = (View) param.args[2];
+                if( ((ViewGroup)view).getChildCount() ==2)  view.setAlpha(0.0f);
+            }
+        });
 
-           }//fter hok
-       });//find and hook method
     }//handleLoadPackage
 }
